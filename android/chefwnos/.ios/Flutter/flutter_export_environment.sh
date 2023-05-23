409f3d3c91fbaf2a5bf2766c6182fdf8fdb06d9d
mohamed886 <61754682+MohamedAmrTaha@users.noreply.github.com>
2023-05-23 15:21:47 +0300

    app setup

--- android/chefwnos/.ios/Flutter/flutter_export_environment.sh
+++ android/chefwnos/.ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter_windows_3.10.1-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\LEGION\Documents\GitHub\ChefwNos\android\chefwnos"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\gp23\ChefwNos\android\chefwnos"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
